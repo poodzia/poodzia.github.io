f8349522e94322e4d0ef35e7142a823838a17329
poodzia <109169765+poodzia@users.noreply.github.com>
2022-07-13 01:27:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+(function(html) {
+
+    'use strict';
+
+   /* Preloader
+    * -------------------------------------------------- */
+    const ssPreloader = function() {
+
+        const body = document.querySelector('body');
+        const preloader = document.querySelector('#preloader');
+
+        if (!(preloader)) return;
+
+        html.classList.add('ss-preload');
+
+        window.addEventListener('load', function() {
+
+            html.classList.remove('ss-preload');
+            html.classList.add('ss-loaded');
+
+            // page scroll position to top
+            preloader.addEventListener('transitionstart', function gotoTop(e) {
+                if (e.target.matches('#preloader')) {
+                    window.scrollTo(0, 0);
+                    preloader.removeEventListener(e.type, gotoTop);
+                }
+            });
+
+            preloader.addEventListener('transitionend', function afterTransition(e) {
+                if (e.target.matches('#preloader'))  {
+                    body.classList.add('ss-show');
+                    e.target.style.display = 'none';
+                    preloader.removeEventListener(e.type, afterTransition);
+                }
+            });
+
+        });
+
+        window.addEventListener('beforeunload' , function() {
+            body.classList.remove('ss-show');
+        });
+    };
+
+
+   /* Swiper
+    * ------------------------------------------------------ */ 
+    const ssSwiper = function() {
+
+        const mySwiper = new Swiper('.swiper-container', {
+
+            slidesPerView: 1,
+            effect: 'fade',
+            speed: 2000,
+            autoplay: {
+                delay: 5000,
+            }
+
+        });
+    };
+
+   /* Smooth Scrolling
+    * ------------------------------------------------------ */
+    const ssMoveTo = function(){
+
+        const easeFunctions = {
+            easeInQuad: function (t, b, c, d) {
+                t /= d;
+                return c * t * t + b;
+            },
+            easeOutQuad: function (t, b, c, d) {
+                t /= d;
+                return -c * t* (t - 2) + b;
+            },
+            easeInOutQuad: function (t, b, c, d) {
+                t /= d/2;
+                if (t < 1) return c/2*t*t + b;
+                t--;
+                return -c/2 * (t*(t-2) - 1) + b;
+            },
+            easeInOutCubic: function (t, b, c, d) {
+                t /= d/2;
+                if (t < 1) return c/2*t*t*t + b;
+                t -= 2;
+                return c/2*(t*t*t + 2) + b;
+            }
+        }
+
+        const triggers = document.querySelectorAll('.smoothscroll');
+        
+        const moveTo = new MoveTo({
+            tolerance: 0,
+            duration: 1200,
+            easing: 'easeInOutCubic',
+            container: window
+        }, easeFunctions);
+
+        triggers.forEach(function(trigger) {
+            moveTo.registerTrigger(trigger);
+        });
+
+    }; 
+
+
+   /* Initialize
+    * ------------------------------------------------------ */
+    (function ssInit() {
+
+        ssPreloader();
+        ssSwiper();
+        ssMoveTo();
+
+    })();
+
+})(document.documentElement);
